9b4de4022ad3e4b5f9320b39c5384d9f27fc97b3
fnaadb <fnaadb@gmail.com>
2025-03-24 12:16:40 -0400

    made sure everything is saved

--- src/chat.py
+++ src/chat.py
@@ -7,6 +7,7 @@ from semantic_kernel.connectors.ai.function_choice_behavior import FunctionChoic
 from semantic_kernel.connectors.openapi_plugin import OpenAPIFunctionExecutionParameters
 from semantic_kernel.contents.chat_history import ChatHistory
 from semantic_kernel.functions import KernelArguments
+from openai import AzureOpenAI
 
 
 #Add logger
@@ -39,9 +40,56 @@ def initialize_kernel():
     logger.info("Chat completion service added to the service")
 
 
+    return kernel
+
+
 
 async def process_message(user_input):
     logger.info(f"Processing user message: {user_input}")
     kernel = initialize_kernel()
+    chat_function = kernel.add_function(
+        prompt="{{$chat_history}}{{$user_input}}",
+        plugin_name="ChatBot",
+        function_name="Chat"
+    )
+
+    #Challenge 03 and 04 - Services Required
+    #Challenge 03 - Create Prompt Execution Settings
+    execution_settings = kernel.get_prompt_execution_settings_from_service_id("chat_completion")
+    execution_settings.function_choice_behavior = FunctionChoiceBehavior.Auto()
+    arguments = KernelArguments(settings=execution_settings)
+
+
+
+
+    # Challenge 03 - Add Time Plugin
+    # Placeholder for Time plugin
+
+    # Challenge 04 - Import OpenAPI Spec
+    # Placeholder for OpenAPI plugin
+
+
+    # Challenge 05 - Add Search Plugin
+
+
+    # Challenge 06- Semantic kernel filters
+
+    # Challenge 07 - Text To Image Plugin
+    # Placeholder for Text To Image plugin
+
+    # Start Challenge 02 - Sending a message to the chat completion service by invoking kernel
+    chat_history.add_user_message(user_input)
+    arguments["user_input"] = user_input
+    arguments["chat_history"] = chat_history
+    result = await kernel.invoke(chat_function, arguments=arguments)
+    chat_history.add_user_message(user_input)
+    chat_history.add_assistant_message(str(result))
+
+
+    return result
+
+def reset_chat_history():
+    global chat_history
+    chat_history = ChatHistory()
 
 
